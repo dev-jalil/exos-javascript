38b759b48ad01029dc6690b39822a303b2ce5a21
SRHIR Abdeljalil <s.abdeljalil2014@gmail.com>
2021-02-13 12:05:37 +0100

    exo3

--- exo3.js
+++ exo3.js
@@ -15,6 +15,9 @@ if(false)points += 5;
 // Exemple 1029
 
 // Code
+let arr = [29, 1029, 9, 12, 23, 31, 98, 121];
+console.log(Math.max(...arr));
+
 
 // console.log(???)
 
@@ -24,6 +27,7 @@ console.log("3.2 Afficher un nombre pseudo-aléatoire compris entre 0 (inclus) e
 if(false)points += 5;
 
 // Code
+console.log(Math.random());
 
 // console.log(???)
 
@@ -34,6 +38,7 @@ console.log("3.3 Afficher l'arrondi supérieur de "+valeur33+" :");
 if(false)points += 5;
 
 // Code
+console.log(Math.ceil(valeur33));
 
 // console.log(???)
 
@@ -44,6 +49,7 @@ console.log("3.4 Afficher l'arrondi inférieur de "+valeur34+" :");
 if(false)points += 5;
 
 // Code
+console.log(Math.floor(valeur34));
 
 // console.log(???)
 
@@ -63,5 +69,16 @@ var cafe = 2.5;
 var champagne = 8.0;
 
 // Code
+let average = (133.5 - (2.5 + 8 * 4)) / 5; // on calcule un premier montant que tout le monde doit payer d'office égal à 133.5 moin les 4 champagnes et le café le tout divisé par 5
+let addition = {};
+for (let i = 1; i <= 5; i++) {
+  addition["personne" + i] = average;
+}
+addition["personne1"] += 2.5;
+for (let i = 1; i < 5; i++) {
+	  addition["personne" + i] += 8;
+
+}
+console.log(addition);
 
 // console.log(???)
